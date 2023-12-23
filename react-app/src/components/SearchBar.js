02bbd1da0cbd993e8e60c5deaab2d17698a7c225
Lakshay <lakshay21060@iiitd.ac.in>
2023-12-23 21:03:05 +0530

    added search box (css just) (does not work as of yet)

---
+++ react-app/src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+import './SearchBar.css';
+
+export const SearchBar = () => {
+  return (
+    <div className="input-wrapper">
+      <input placeholder="Type to search..."/>
+    </div>
+  );
+} 
